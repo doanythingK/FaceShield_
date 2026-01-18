c597caeeb265196c32a672389c81b644d7b30e19
root <root@KangPC.>
2026-01-18 21:27:34 +0900

    Configure FFmpeg root path on startup

--- Services/Video/FFmpegBootstrap.cs
+++ Services/Video/FFmpegBootstrap.cs
@@ -1,6 +1,7 @@
 using FFmpeg.AutoGen;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace FaceShield.Services.Video
         {
             try
             {
+                ConfigureRootPath();
                 ffmpeg.avcodec_version();
                 Console.WriteLine("[FFmpeg] Native libraries loaded successfully.");
             }
@@ -21,6 +23,23 @@ namespace FaceShield.Services.Video
                 throw new InvalidOperationException(
                     "FFmpeg native libraries not loaded. Check DLL placement.",
                     ex);
+            }
+        }
+
+        private static void ConfigureRootPath()
+        {
+            var baseDir = AppContext.BaseDirectory;
+            var macFrameworks = Path.GetFullPath(Path.Combine(baseDir, "..", "Frameworks"));
+            if (Directory.Exists(macFrameworks))
+            {
+                ffmpeg.RootPath = macFrameworks;
+                return;
+            }
+
+            var ffmpegDir = Path.Combine(baseDir, "FFmpeg");
+            if (Directory.Exists(ffmpegDir))
+            {
+                ffmpeg.RootPath = ffmpegDir;
             }
         }
     }
